2ba23e8faadc66597f33a104594524c8c854c84c
Doni Abeje <abeje.doni@gmail.com>
2021-07-21 21:54:32 +0300

    inject question and answer models into qa service

--- src/qa/qa.service.ts
+++ src/qa/qa.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Answer, AnswerDocument } from './schema/answer.schema';
+import { Question, QuestionDocument } from './schema/question.schema';
 
 @Injectable()
-export class QaService {}
+export class QaService {
+  constructor(
+    @InjectModel(Question.name) public questionModel: Model<QuestionDocument>,
+    @InjectModel(Answer.name) public answerModel: Model<AnswerDocument>,
+  ) {}
+
+
+
+}
