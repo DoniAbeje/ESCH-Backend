485146ac28cfea9f6a17c7a9e0b2332b1107f665
Doni Abeje <abeje.doni@gmail.com>
2021-07-22 19:22:33 +0300

    implement answer question endpoint

--- src/qa/qa.controller.ts
+++ src/qa/qa.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, Param } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { User } from 'src/utils/user.decorator';
+import { AnswerQuestionDto } from './dto/answer-question.dto';
 import { RaiseQuestionDto } from './dto/raise-question.dto';
 import { QaService } from './qa.service';
 
@@ -25,7 +26,22 @@ export class QaController {
 
   @ApiTags('Fetch Questions')
   @Get('/')
-  async fetchAllQuestions(){
+  async fetchAllQuestions() {
     return this.qaService.findAllQuestions();
+  }
+
+  @ApiTags('Answer Question')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Post('/:questionId/answer')
+  async answerQuestion(
+    @Body() answerQuestionDto: AnswerQuestionDto,
+    @User() user,
+    @Param('questionId') questionId: string,
+  ) {
+    answerQuestionDto.answeredBy = user.id;
+    answerQuestionDto.question = questionId;
+    const answer = await this.qaService.answerQuestion(answerQuestionDto);
+    return { _id: answer._id };
   }
 }
--- src/qa/qa.service.ts
+++ src/qa/qa.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { AnswerQuestionDto } from './dto/answer-question.dto';
 import { RaiseQuestionDto } from './dto/raise-question.dto';
+import { QuestionDoesNotExistException } from './exceptions/question-doesnot-exist.exception';
 import { Answer, AnswerDocument } from './schema/answer.schema';
 import { Question, QuestionDocument } from './schema/question.schema';
 
@@ -29,5 +31,18 @@ export class QaService {
         },
       },
     ]);
+  }
+
+  async answerQuestion(answerQuestionDto: AnswerQuestionDto) {
+    await this.findQuestionById(answerQuestionDto.question);
+    return this.answerModel.create(answerQuestionDto);
+  }
+
+  async findQuestionById(id: string) {
+    const question = await this.questionModel.findById(id);
+    if (!question) {
+      throw new QuestionDoesNotExistException();
+    }
+    return question;
   }
 }
