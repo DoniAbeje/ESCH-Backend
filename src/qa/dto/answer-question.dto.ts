2774b015ddbc076aa337d7afd49f79d9ed31c90d
Doni Abeje <abeje.doni@gmail.com>
2021-07-22 18:58:50 +0300

    add  question property to answer-question dto

--- src/qa/dto/answer-question.dto.ts
+++ src/qa/dto/answer-question.dto.ts
@@ -1,10 +1,13 @@
-import { ApiHideProperty } from "@nestjs/swagger";
-import { IsString } from "class-validator";
+import { ApiHideProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 
 export class AnswerQuestionDto {
-    @IsString()
-    readonly answer: string
+  @IsString()
+  readonly answer: string;
 
-    @ApiHideProperty()
-    answeredBy: string
+  @ApiHideProperty()
+  answeredBy: string;
+
+  @ApiHideProperty()
+  question: string;
 }
