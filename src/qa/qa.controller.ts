76db8f03fbcc4e99279109ec7a1a10649fbd5a1c
Doni Abeje <abeje.doni@gmail.com>
2021-07-22 13:55:35 +0300

    implement raise-question

--- src/qa/qa.controller.ts
+++ src/qa/qa.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { User } from 'src/utils/user.decorator';
+import { RaiseQuestionDto } from './dto/raise-question.dto';
+import { QaService } from './qa.service';
 
-@Controller('qa')
-export class QaController {}
+@ApiTags('Question and Answer')
+@Controller('question')
+export class QaController {
+  constructor(private qaService: QaService) {}
+
+  @ApiTags('Raise Question')
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Post('/')
+  async raiseQuestion(
+    @Body() raiseQuestionDto: RaiseQuestionDto,
+    @User() user,
+  ) {
+    raiseQuestionDto.askedBy = user.id;
+    const question = await this.qaService.raiseQuestion(raiseQuestionDto);
+    return { _id: question._id };
+  }
+}
--- src/qa/qa.service.ts
+++ src/qa/qa.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { RaiseQuestionDto } from './dto/raise-question.dto';
 import { Answer, AnswerDocument } from './schema/answer.schema';
 import { Question, QuestionDocument } from './schema/question.schema';
 
@@ -11,6 +12,7 @@ export class QaService {
     @InjectModel(Answer.name) public answerModel: Model<AnswerDocument>,
   ) {}
 
-
-
+  async raiseQuestion(raiseQuestionDto: RaiseQuestionDto) {
+    return await this.questionModel.create(raiseQuestionDto);
+  }
 }
