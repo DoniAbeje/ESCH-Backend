b846e90e65547405ea766aae42133e237ca0256a
Doni Abeje <abeje.doni@gmail.com>
2021-07-23 11:04:27 +0300

    change QuestionDoesNotExistException 404 exception

--- src/qa/exceptions/question-doesnot-exist.exception.ts
+++ src/qa/exceptions/question-doesnot-exist.exception.ts
@@ -1,6 +1,6 @@
-import { BadRequestException } from '@nestjs/common';
+import { NotFoundException } from '@nestjs/common';
 
-export class QuestionDoesNotExistException extends BadRequestException {
+export class QuestionDoesNotExistException extends NotFoundException {
   constructor(message?: string) {
     message = message ? message : "Question doesn't exist";
     super({
