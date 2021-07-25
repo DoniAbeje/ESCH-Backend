0cec70a258f09cd4d61d7c38b08fcc6df9f8f6f0
robel <robelfekadu2017@gmail.com>
2021-07-25 17:18:46 +0300

    add exam question schema

---
+++ src/exam/schema/exam-question.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema } from '@nestjs/mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+
+export type ExamQuestionDocument = ExamQuestion & Document;
+
+@Schema()
+export class ExamQuestion {
+  @Prop({ required: true })
+  question: string;
+
+  @Prop({ type: [{ key: String, choice: String }], minlength: 1 })
+  choice: object[];
+
+  @Prop()
+  explanation: string;
+
+  @Prop({ required: true })
+  correctAnswer: string;
+
+  @Prop({ type: MongooseSchema.Types.ObjectId, required: true })
+  examId: string;
+}
