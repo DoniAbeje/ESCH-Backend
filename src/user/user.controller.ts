47e2c8d5ee45bc58c00517836f6f92246e30906d
Doni Abeje <abeje.doni@gmail.com>
2021-07-27 22:07:58 +0300

    fix lint warnings

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,20 +1,11 @@
-import { JwtAuthGuard } from './../auth/guards/jwt-auth.guard';
 import { LocalAuthGuard } from './../auth/guards/local-auth.guard';
 import { AuthService } from './../auth/auth.service';
 import { UserDocument } from './schemas/user.schema';
-import {
-  Controller,
-  Post,
-  Body,
-  Put,
-  Param,
-  Get,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Post, Body, Param, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
 import { User } from '../common/decorators/user.decorator';
 import { LoginDto } from './dto/login.dto';
 import { PutAuth } from 'src/common/decorators/put-auth.decorator';
