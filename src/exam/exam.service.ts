26625e87bf748c62be65e245c7bcfc3622a0ea55
robelfekadu <robel.fekadu9@gmail.com>
2021-08-02 14:36:44 +0300

    add create exam method in exam service

--- src/exam/exam.service.ts
+++ src/exam/exam.service.ts
@@ -2,8 +2,13 @@ import { Exam, ExamDocument } from './schema/exam.schema';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { CreateExamDto } from './dto/create-exam.dto';
 
 @Injectable()
 export class ExamService {
   constructor(@InjectModel(Exam.name) public examModel: Model<ExamDocument>) {}
+
+  async createExam(createExamDto: CreateExamDto) {
+    return await this.examModel.create(createExamDto);
+  }
 }
