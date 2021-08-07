1124015b61cd523ddba3d69aab70e8101c34376d
Doni Abeje <abeje.doni@gmail.com>
2021-08-07 19:12:33 +0300

    test create-user endpoint with success

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -6,10 +6,12 @@ import { configApp } from '../src/utils/config-app';
 import { TestHelperService } from '../src/common/services/test-helper.service';
 import { CreateUserDto } from '../src/user/dto/create-user.dto';
 import { PhoneTakenException } from '../src/user/exceptions/phone-taken.exception';
+import { UserService } from '../src/user/user.service';
 
 describe('User Module (e2e)', () => {
   let app: INestApplication;
   let helper: TestHelperService;
+  let userService: UserService;
   const baseUrl = '/users';
 
   beforeAll(async () => {
@@ -19,6 +21,7 @@ describe('User Module (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     helper = moduleFixture.get<TestHelperService>(TestHelperService);
+    userService = moduleFixture.get<UserService>(UserService);
     configApp(app);
 
     await app.init();
@@ -26,7 +29,7 @@ describe('User Module (e2e)', () => {
 
   beforeEach(async () => {
     await helper.clearUsersData();
-  })
+  });
 
   afterAll(async () => {
     await app.close();
@@ -58,6 +61,33 @@ describe('User Module (e2e)', () => {
         .expect(HttpStatus.BAD_REQUEST);
 
       expect(body.exception).toEqual(PhoneTakenException.name);
+    });
+
+    it('should create new user', async () => {
+      const createUserDto: CreateUserDto = helper.generateCreateUserDto();
+
+      const { body } = await request(app.getHttpServer())
+        .post(baseUrl)
+        .send(createUserDto)
+        .expect(HttpStatus.CREATED);
+
+      expect(body).toEqual({
+        token: expect.any(String),
+        userInfo: {
+          _id: expect.any(String),
+          firstName: createUserDto.firstName,
+          lastName: createUserDto.lastName,
+          phone: createUserDto.phone,
+        },
+      });
+
+      const savedUser = await userService.exists(body.userInfo._id);
+      const { firstName, lastName, phone } = createUserDto;
+      const expectedSavedData = { firstName, lastName, phone };
+
+      expect(savedUser).toMatchObject(
+        expect.objectContaining(expectedSavedData),
+      );
     });
   });
 });
