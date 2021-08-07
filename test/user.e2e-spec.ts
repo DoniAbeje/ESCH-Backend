632ce6a00f0a1603f89c389f54e657a9a8966686
Doni Abeje <abeje.doni@gmail.com>
2021-08-07 18:28:49 +0300

    test create-user endpoint for dup phone

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -3,23 +3,30 @@ import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { configApp } from '../src/utils/config-app';
-import { validate } from 'class-validator';
+import { TestHelperService } from '../src/common/services/test-helper.service';
 import { CreateUserDto } from '../src/user/dto/create-user.dto';
+import { PhoneTakenException } from '../src/user/exceptions/phone-taken.exception';
 
 describe('User Module (e2e)', () => {
   let app: INestApplication;
+  let helper: TestHelperService;
   const baseUrl = '/users';
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    helper = moduleFixture.get<TestHelperService>(TestHelperService);
     configApp(app);
 
     await app.init();
   });
+
+  beforeEach(async () => {
+    await helper.clearUsersData();
+  })
 
   afterAll(async () => {
     await app.close();
@@ -39,6 +46,18 @@ describe('User Module (e2e)', () => {
         .expect(HttpStatus.BAD_REQUEST);
 
       expect(body.message).toEqual(expectedMessage);
+    });
+
+    it(`should reject with ${PhoneTakenException.name}`, async () => {
+      await helper.createTestUser();
+      const createUserDto: CreateUserDto = helper.generateCreateUserDto();
+
+      const { body } = await request(app.getHttpServer())
+        .post(baseUrl)
+        .send(createUserDto)
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(body.exception).toEqual(PhoneTakenException.name);
     });
   });
 });
