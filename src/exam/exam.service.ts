28f60c9bf4f52aad9ac6300400a9e257f08490b7
robelfekadu <robel.fekadu9@gmail.com>
2021-08-12 10:54:30 +0300

    add fetchOne exam base functionality

--- src/exam/exam.service.ts
+++ src/exam/exam.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateExamDto } from './dto/create-exam.dto';
+import { ExamDoesNotExistException } from './exceptions/exam-doesnot-exist.exception';
 
 @Injectable()
 export class ExamService {
@@ -14,5 +15,19 @@ export class ExamService {
 
   async fetchAll() {
     return this.examModel.find({});
+  }
+
+  async fetchOne(examId: string) {
+    return this.exists(examId);
+  }
+
+  async exists(examId: string) {
+    const exam = await this.examModel.findById(examId);
+
+    if (!exam) {
+      throw new ExamDoesNotExistException();
+    }
+
+    return exam;
   }
 }
