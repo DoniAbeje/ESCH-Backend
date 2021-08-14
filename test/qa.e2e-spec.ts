6de800384bd3cb27d498e669b07cee50df8d5f55
Doni Abeje <abeje.doni@gmail.com>
2021-08-14 16:58:09 +0300

    test raise question endpoint for authentication

--- test/qa.e2e-spec.ts
+++ test/qa.e2e-spec.ts
@@ -1,13 +1,14 @@
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { QaTestHelperService } from '../src/qa/test-helper.service';
 import { configApp } from '../src/utils/config-app';
+import * as request from 'supertest';
 
 describe('QA Module (e2e)', () => {
   let app: INestApplication;
   let qaTestHelper: QaTestHelperService;
-  const baseUrl = '/qa';
+  const baseUrl = '/question';
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -30,4 +31,14 @@ describe('QA Module (e2e)', () => {
   afterAll(async () => {
     await app.close();
   });
+
+  describe('raiseQuestion', () => {
+    // check authentication
+    it('should reject with unauthenticated user', async () => {
+        await request(app.getHttpServer())
+        .post(baseUrl)
+        .expect(HttpStatus.UNAUTHORIZED)
+    })
+
+  })
 });
