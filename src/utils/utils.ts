ed14e487ba8a03993fd9d784f1d23fd8841e3222
Doni Abeje <abeje.doni@gmail.com>
2021-08-14 18:09:39 +0300

    add doc comment to toJSON function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,7 @@
 import { Document } from 'mongoose';
-
+/**
+Converts Mongoose Document to javascript POJO
+*/
 export function toJSON(doc: Document) {
   return JSON.parse(JSON.stringify(doc.toObject()));
 }
