7d4bde90c40306d6bb2e01f6302bca7141d2dbd7
Doni Abeje <abeje.doni@gmail.com>
2021-08-14 17:08:16 +0300

    test raise question endpoint for input validation

--- test/qa.e2e-spec.ts
+++ test/qa.e2e-spec.ts
@@ -4,10 +4,14 @@ import { AppModule } from '../src/app.module';
 import { QaTestHelperService } from '../src/qa/test-helper.service';
 import { configApp } from '../src/utils/config-app';
 import * as request from 'supertest';
+import { UserTestHelperService } from '../src/user/test-helper.service';
+import { AuthService } from '../src/auth/auth.service';
 
 describe('QA Module (e2e)', () => {
   let app: INestApplication;
   let qaTestHelper: QaTestHelperService;
+  let userTestHelper: UserTestHelperService;
+  let authService: AuthService;
   const baseUrl = '/question';
 
   beforeAll(async () => {
@@ -18,6 +22,8 @@ describe('QA Module (e2e)', () => {
     app = moduleFixture.createNestApplication();
     qaTestHelper =
       moduleFixture.get<QaTestHelperService>(QaTestHelperService);
+    userTestHelper = moduleFixture.get<UserTestHelperService>(UserTestHelperService);
+    authService = moduleFixture.get<AuthService>(AuthService);
     configApp(app);
 
     await app.init();
@@ -26,6 +32,7 @@ describe('QA Module (e2e)', () => {
   beforeEach(async () => {
     await qaTestHelper.clearAnswers();
     await qaTestHelper.clearQuestions();
+    await userTestHelper.clearUsersData();
   });
 
   afterAll(async () => {
@@ -40,5 +47,20 @@ describe('QA Module (e2e)', () => {
         .expect(HttpStatus.UNAUTHORIZED)
     })
 
+    it('should reject with input validation', async () => {
+        const user = await userTestHelper.createTestUser();
+        const token = await authService.signToken(user);
+        const expectedMessage = [
+            'question must be a string',
+            'tags must contain at least 1 elements'
+          ]
+
+        const { body } = await request(app.getHttpServer())
+        .post(baseUrl)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(HttpStatus.BAD_REQUEST)
+
+        expect(body.message).toEqual(expectedMessage);
+    })
   })
 });
