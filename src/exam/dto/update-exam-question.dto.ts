af2708401e4d2b7f5c3107df45b32e68c7a6f000
Doni Abeje <abeje.doni@gmail.com>
2021-08-19 11:11:11 +0300

    make UpdateExamQuestionDto props optional

--- src/exam/dto/update-exam-question.dto.ts
+++ src/exam/dto/update-exam-question.dto.ts
@@ -5,19 +5,19 @@ import { Choice } from './add-exam-question.dto';
 export class UpdateExamQuestionDto {
   @IsString()
   @IsOptional()
-  readonly question: string;
+  readonly question?: string;
 
   @IsArray()
   @ValidateNested({ each: true })
   @Type(() => Choice)
   @IsOptional()
-  readonly choices: Choice[];
+  readonly choices?: Choice[];
 
   @IsString()
   @IsOptional()
-  readonly explanation: string;
+  readonly explanation?: string;
 
   @IsString()
   @IsOptional()
-  readonly correctAnswer: string;
+  readonly correctAnswer?: string;
 }
