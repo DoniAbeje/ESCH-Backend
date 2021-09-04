b9009e0ce241af30e4b796ba0eb24c978d0949df
Robel.Fekadu <robel@peoplise.com>
2021-09-04 16:17:20 +0300

    add exam-sale-query-builder

---
+++ src/exam/query/exam-sale-query-builder.ts
@@ -0,0 +1,86 @@
+import { PaginationOption } from '../../common/pagination-option';
+import * as mongoose from 'mongoose';
+import { ExecResult } from '../../qa/query/exec-result';
+import { ExamSaleDocument } from '../schema/exam-sale.schema';
+
+export class ExamSaleQueryBuilder {
+  private isBuilt = false;
+  private aggregations: any[] = [];
+  private paginationOption: PaginationOption;
+  private buyerFilters: string[] = [];
+  private shouldPopulateExam = false;
+
+  constructor(private examSaleModel: mongoose.Model<ExamSaleDocument>) {}
+
+  paginate(paginationOption: PaginationOption) {
+    this.paginationOption = paginationOption;
+    return this;
+  }
+
+  filterByBuyers(buyers: string[]) {
+    this.buyerFilters = buyers;
+    return this;
+  }
+
+  populateExam(shouldPopulateExam = true) {
+    this.shouldPopulateExam = shouldPopulateExam;
+    return this;
+  }
+
+  build() {
+    const match: ExamSaleMatchQuery = this.processFilter();
+
+    if (match != {}) {
+      this.aggregations.push({
+        $match: match,
+      });
+    }
+    // limit
+    if (this.paginationOption) {
+      this.aggregations.push({
+        $skip: this.paginationOption.offset,
+      });
+
+      this.aggregations.push({
+        $limit: this.paginationOption.limit,
+      });
+    }
+
+    this.isBuilt = true;
+    return this.aggregations;
+  }
+
+  async exec() {
+    if (!this.isBuilt) {
+      this.build();
+    }
+
+    let examSales = await this.examSaleModel.aggregate(this.aggregations);
+    examSales = await this.processPopulate(examSales);
+
+    return new ExecResult(examSales);
+  }
+
+  private processFilter(): ExamSaleMatchQuery {
+    const match: ExamSaleMatchQuery = {};
+
+    if (this.buyerFilters.length) {
+      match.buyer = { $in: this.buyerFilters };
+    }
+    return match;
+  }
+
+  private async processPopulate(examSales: any[]) {
+    if (this.shouldPopulateExam) {
+      examSales = await this.examSaleModel.populate(examSales, {
+        path: 'exam',
+        select: ['_id', 'title', 'description', 'price', 'tags'],
+      });
+    }
+    return examSales;
+  }
+}
+
+class ExamSaleMatchQuery {
+  buyer?: any;
+}
