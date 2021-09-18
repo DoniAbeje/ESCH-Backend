ef3ad0242ef0c622c7123ee746aaf5f41f9e0a24
Robel.Fekadu <robel@peoplise.com>
2021-09-18 11:53:45 +0300

    implement base setup method for question recommendation service

--- src/qa/question-recommendation.service.ts
+++ src/qa/question-recommendation.service.ts
@@ -1,11 +1,12 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { TfIdf } from 'natural';
 import { QuestionService } from './question.service';
+import { QuestionDocument } from './schema/question.schema';
 
 @Injectable()
 export class QuestionRecommendationService {
-  private fromExamIdToIndex = {};
-  private fromIndexToExamId = {};
+  private fromQuestionIdToIndex = {};
+  private fromIndexToQuestionId = {};
   private tfidf: TfIdf;
   private needsSetup = true;
 
@@ -14,5 +15,35 @@ export class QuestionRecommendationService {
     private questionService: QuestionService,
   ) {
     this.tfidf = new TfIdf();
+  }
+
+  async setup(newQuestionAdded = false) {
+    const questions: QuestionDocument[] = await this.questionService.fetchAll(
+      null,
+    );
+
+    if (newQuestionAdded || this.needsSetup) {
+      this.fromQuestionIdToIndex = {};
+      this.fromIndexToQuestionId = {};
+      this.tfidf = new TfIdf();
+
+      questions.forEach((question, index) => {
+        this.tfidf.addDocument(this.constructQuestionDocument(question));
+        this.fromQuestionIdToIndex[question._id] = index;
+        this.fromIndexToQuestionId[index] = question._id;
+      });
+      this.needsSetup = false;
+    }
+
+    return questions;
+  }
+
+  private constructQuestionDocument(question: QuestionDocument) {
+    let questionDoc = question.question;
+    question.tags.forEach((tag) => {
+      questionDoc += ` ${tag}`;
+    });
+
+    return questionDoc;
   }
 }
